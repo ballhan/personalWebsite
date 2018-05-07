61b113dfd62d9686cc49aa8271423436c767f2a2
Bohan Chen <bohanchen81@yahoo.com>
2018-05-07 16:26:44 -0400

    scrolling js

--- js/scrolling.js
+++ js/scrolling.js
@@ -1,26 +1,15 @@
 $(document).ready(function(){
-  // Add smooth scrolling to all links
-  var scrollLink = $('.scroll');
+  
+  $('.scroll').click(function(e){
 
-  scrollLink.click(function(e)){
-    e.preventDefault();
-    $('body,html').animate({
-      scrollTop: $(this.hash).offset().top
-    }, 500);
-    })
+    
+    var linkHref = $(this).attr('href');
+    $('html,body').animate({
+      scrollTop: $(linkHref).offset().top 
+     }, "slow"); 
 
-  // Link switching
-  $(window).scroll(function(){
-    var scrollbarLocation = $(this).scrollTop();
+    e.preventDefault(); 
 
-    scrollLink.each(function(){
-      var sectionOffset = $(this.hash).offset().top - 20;
+  });
 
-      if(sectionOffset <= scrollbarLocation){
-        $(this).parent().addClass('active');
-        $(this).parent().siblings(removeClass('active'))
-      }
-   })
-  })
-
-})
+ }); 
