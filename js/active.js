71fc3b217b4e1bc237ab68df033d23ebf1e7bc5f
Bohan Chen <bohanchen81@yahoo.com>
2018-07-05 12:45:10 -0400

    delete active on click

--- js/active.js
+++ js/active.js
@@ -1,11 +1,3 @@
-$(function () {
-    $(".nav a").on("click", function(){
-        $(".nav").find(".active").removeClass("active");
-        $(this).parent().addClass("active");
-    });
-});
-
-
 $(function() {
   documentEl = $(document);
   documentEl.on('scroll', function() {
