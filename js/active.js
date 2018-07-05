7aba4286fd6c8afc19957d53d2afccd87a9e678a
Bohan Chen <bohanchen81@yahoo.com>
2018-07-05 12:13:44 -0400

    fixes #106

--- js/active.js
+++ js/active.js
@@ -4,3 +4,36 @@ $(function () {
         $(this).parent().addClass("active");
     });
 });
+
+
+$(function() {
+  documentEl = $(document);
+  documentEl.on('scroll', function() {
+    if (isScrolledIntoView('#top-indi')) {
+        $(".nav").find(".active").removeClass("active");
+        $('#top-nav').parent().addClass("active");
+    };
+
+    if (isScrolledIntoView('#portfolio-indi')) {
+        $(".nav").find(".active").removeClass("active");
+        $('#portfolio-nav').parent().addClass("active");
+    };
+    if (isScrolledIntoView('#portfolio-indi2')) {
+        $(".nav").find(".active").removeClass("active");
+        $('#portfolio-nav').parent().addClass("active");
+    };
+    if (isScrolledIntoView('#about-indi')) {
+        $(".nav").find(".active").removeClass("active");
+        $('#about-nav').parent().addClass("active");
+    };
+    if (isScrolledIntoView('#about-indi2')) {
+        $(".nav").find(".active").removeClass("active");
+        $('#about-nav').parent().addClass("active");
+    };
+    if (isScrolledIntoView('#contact-indi')) {
+        $(".nav").find(".active").removeClass("active");
+        $('#contact-nav').parent().addClass("active");
+    };
+
+  });
+});
