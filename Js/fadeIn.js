90b4dc929cd4bd8539589b47fbbfdfea37cac9ce
Bohan Chen <bohanchen81@yahoo.com>
2018-09-06 09:18:04 -0400

    fade

--- Js/fadeIn.js
+++ Js/fadeIn.js
@@ -3,61 +3,45 @@ $(function() {
   documentEl.on('scroll', function() {	
     if (isScrolledIntoView('#imgKik')) {	
         $('#imgKik').addClass("content-fade");	
-        $('#imgKik').removeClass("temp-hide");	
+        $('#imgKik').removeClass("temp-hide");
+        $('#textKik').addClass("content-fade");	
+        $('#textKik').removeClass("temp-hide");		
     };	
     if (isScrolledIntoView('#imgUWCSA')) {	
         $('#imgUWCSA').addClass("content-fade");	
-        $('#imgUWCSA').removeClass("temp-hide");	
+        $('#imgUWCSA').removeClass("temp-hide");
+        $('#textUWCSA').addClass("content-fade");	
+        $('#textUWCSA').removeClass("temp-hide");		
     };	
     if (isScrolledIntoView('#imgMonsheong')) {	
         $('#imgMonsheong').addClass("content-fade");	
-        $('#imgMonsheong').removeClass("temp-hide");	
-    };	
-    if (isScrolledIntoView('#textKik')) {	
-        $('#textKik').addClass("content-fade");	
-        $('#textKik').removeClass("temp-hide");	
-    };	
-    if (isScrolledIntoView('#imgUWCSA')) {	
-        $('#textUWCSA').addClass("content-fade");	
-        $('#textUWCSA').removeClass("temp-hide");	
-    };	
-    if (isScrolledIntoView('#imgMonsheong')) {	
+        $('#imgMonsheong').removeClass("temp-hide");
         $('#textMonsheong').addClass("content-fade");	
-        $('#textMonsheong').removeClass("temp-hide");	
+        $('#textMonsheong').removeClass("temp-hide");		
     };	
     if (isScrolledIntoView('#imgBro')) {	
         $('#imgBro').addClass("content-fade");	
         $('#imgBro').removeClass("temp-hide");	
+        $('#textBro').addClass("content-fade");	
+        $('#textBro').removeClass("temp-hide");	
     };
     if (isScrolledIntoView('#imgSoccer')) {	
         $('#imgSoccer').addClass("content-fade");	
-        $('#imgSoccer').removeClass("temp-hide");	
+        $('#imgSoccer').removeClass("temp-hide");
+        $('#textSoccer').addClass("content-fade");	
+        $('#textSoccer').removeClass("temp-hide");		
     };		
     if (isScrolledIntoView('#imgCSA')) {	
         $('#imgCSA').addClass("content-fade");	
-        $('#imgCSA').removeClass("temp-hide");	
+        $('#imgCSA').removeClass("temp-hide");
+        $('#textCSA').addClass("content-fade");	
+        $('#textCSA').removeClass("temp-hide");		
     };	
     if (isScrolledIntoView('#videoDance')) {	
         $('#videoDance').addClass("content-fade");	
-        $('#videoDance').removeClass("temp-hide");	
-    };	
-    if (isScrolledIntoView('#textBro')) {	
-        $('#textBro').addClass("content-fade");	
-        $('#textBro').removeClass("temp-hide");	
-    };	
-    if (isScrolledIntoView('#textSoccer')) {	
-        $('#textSoccer').addClass("content-fade");	
-        $('#textSoccer').removeClass("temp-hide");	
-    };		
-    if (isScrolledIntoView('#textCSA')) {	
-        $('#textCSA').addClass("content-fade");	
-        $('#textCSA').removeClass("temp-hide");	
-    };	
-    if (isScrolledIntoView('#textDance')) {	
+        $('#videoDance').removeClass("temp-hide");
         $('#textDance').addClass("content-fade");	
-        $('#textDance').removeClass("temp-hide");	
+        $('#textDance').removeClass("temp-hide");		
     };	
-
-
   });	
 });
